59b1e99cecaebc29cb0f78401f8d44453c6dface
hansir <827278194@qq.com>
2018-11-12 22:48:54 +0800

    ssl 证书认证，忽略不信任证书

--- 04_t.py
+++ 04_t.py
@@ -1,10 +1,15 @@
 from urllib.request import Request, urlopen
+import ssl
 
 ua = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75Safari/537.36"
 request = Request('http://www.12306.cn/morhweb/')
 request.add_header('user-agent', ua)
-with urlopen(request) as res:
+
+# 忽略不信任的证书
+context = ssl._create_unverified_context()
+with urlopen(request, context=context) as res:
 # res = urlopen(request)
 # with res:
     print(res._method)
+    print(res.geturl())
     print(res.read().decode())
